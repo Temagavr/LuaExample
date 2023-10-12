d80f16ee854a8cd9282c2b1ca696826ad480007a
temagavr <temagavr22@gmail.com>
2023-10-12 18:31:36 +0300

    Добавил примеры

---
+++ lua_scripts/delete.lua
@@ -0,0 +1,51 @@
+-- delete.lua
+local id = ngx.var.arg_id  -- получим параметр из GET запроса
+
+ngx.say("<h3>Delete example</h3>")
+ngx.say("<h4>Id = ", id, "</h4><br/><br/>")
+
+local mysql = require "resty.mysql" -- подключим библиотеку по работе с mysql
+local db, err = mysql:new()
+if not db then
+    ngx.say("failed to instantiate mysql: ", err)
+    return
+end
+
+db:set_timeout(1000) -- 1 sec
+
+local ok, err, errcode, sqlstate = db:connect{
+    host = "127.0.0.1",
+    port = 3306,
+    database = "office",
+    user = "root",
+    password = "MySqlPass",
+    charset = "utf8",
+    max_packet_size = 1024 * 1024,
+}
+
+if not ok then
+    ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
+    return
+end
+
+ngx.say("<h4>connected to mysql.</h4><br/><br/>")
+
+-- run a select query, expected about 10 rows in
+-- the result set:
+res, err, errcode, sqlstate = db:query("delete from employee where id_employee = " .. id)
+if not res then
+    ngx.say("bad result: ", err, ": ", errcode, ": ", sqlstate, ".")
+    return
+end
+
+local cjson = require "cjson"
+ngx.say("result: ", cjson.encode(res))
+-- put it into the connection pool of size 100,
+-- with 10 seconds max idle timeout
+local ok, err = db:set_keepalive(10000, 100)
+if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    return
+end
+
+
